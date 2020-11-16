d54ad3257109909591f4c29d763d3e805013f904
Utilisateur <Utilisateur@Noel-Sam>
2020-11-16 10:17:02 +0100

    Ajout de toutes les classes

--- Shape-Up/src/jeu/Joueur.java
+++ Shape-Up/src/jeu/Joueur.java
@@ -1,10 +1,33 @@
 package jeu;
 
 public class Joueur {
+	
+	String nomJoueur;
+	// int numJoueur; ? Pour avoir une circulation dans la partie 1 --> 3
+    boolean commence;
+    Carte carteVictoire;
+    int [] [] score;
+    
+    public Joueur(String nomJoueur, boolean commence, Carte carteVictoire, int [][] score) {
+    	
+    }
+    
+    public void poserCarte() {
+    	
+    }
+    
+    public void deplacerCarte() {
+    	
+    }
+    
+    public void piocherCarte() {
+    	
+    }
+    
+   
+    
+	
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	
 
 }
---
+++ Shape-Up/src/jeu/Partie.java
@@ -0,0 +1,15 @@
+package jeu;
+
+public class Partie {
+
+	int nombreDeJoueur;
+	int round;
+	int tour;
+	String règle;
+	// est ce que Round et tour sont obligatoire ? tour peut etre créé a l'intérieur du main ? a quoi correspond réellement un round ? 
+	
+	public Partie(int nombreDeJoueur, int round, int tour, String règle) {
+		
+	}
+
+}
