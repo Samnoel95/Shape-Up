4b22fe873797ee67af6dae31fbd1a2ea8f9b5f24
Utilisateur <Utilisateur@Noel-Sam.home>
2021-01-09 15:01:58 +0100

    buttoncard

--- Shape-Up/src/fr/utt/lo02/shapeUp/vue/ButtonCard.java
+++ Shape-Up/src/fr/utt/lo02/shapeUp/vue/ButtonCard.java
@@ -2,6 +2,8 @@ package fr.utt.lo02.shapeUp.vue;
 
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.ImageObserver;
 import java.awt.image.ImageProducer;
 
@@ -17,6 +19,17 @@ public class ButtonCard extends JButton {
 	
 	public ButtonCard(int x, int y) {
 		this.positionCarte = new PositionCarte(x, y);
+		addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("x="+positionCarte.getX());
+				System.out.println("y="+positionCarte.getY());
+			}
+		});
+				
+			
+
 		
 	}
 
